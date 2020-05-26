d2ba87a614bd1acae01e04bf26e70f82413c0f3c
Mateusz Rzeczyca Win <mateuszrzeczyca17@gmail.com>
2020-05-26 18:37:47 +0200

    added matrices computations

--- main.cpp
+++ main.cpp
@@ -3,23 +3,17 @@
  *	Copyright (C) 2020 Mateusz Rzeczyca <info@mateuszrzeczyca.pl>
  */
 
-#include "src/maths.h"
+#include "MARMaths.h"
 
 int main() {
     using namespace mar::maths;
 
-	vec4 v(3.0f, 5.0f, 2.0f, 1.5f);
-	vec4 v2(2.0f, 2.0f, 2.0f, 2.0f);
-	vec4 v3(1.0f, 2.0f, 2.0f, 2.5f);
+    mat4 ide = mat4::identity();
+    ide = mat4(2.0f);
+    mat4 diag(3.0f);
 
-	std::cout << v << std::endl;
+    std::cout << ide << std::endl;
+    std::cout << diag << std::endl;
 
-	v *= v2;
-
-	std::cout << v2 << std::endl;
-	std::cout << v3 << std::endl;
-	std::cout << " --- " << std::endl;
-	std::cout << v << std::endl;
-	std::cout << (v3 != v2) << std::endl;
-	std::cout << (v3 == v2) << std::endl;
+    std::cout << ide * diag << std::endl;
 }
--- src/maths.h
+++ src/maths.h
@@ -6,17 +6,28 @@
 #ifndef MAR_MATH_H
 #define MAR_MATH_H
 
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <iostream>
+
+#include "maths_functions.h"
+
 #include "vec/vec2.h"
 #include "vec/vec3.h"
 #include "vec/vec4.h"
 
+#include "mat/mat4.h"
+
 namespace mar { 
     namespace maths {
+
+        struct Angles;
 
 		struct vec2;
 		struct vec3;
 		struct vec4;
+
+        struct mat4;
 
 } }
 
