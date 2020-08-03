90b1f72941ba3c723a4273e4129ba67affaaa5c2
Mateusz Rzeczyca Win <mateuszrzeczyca17@gmail.com>
2020-08-03 19:47:01 +0200

    worked on cmake

--- include/MARMaths.h
+++ include/MARMaths.h
@@ -9,14 +9,14 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-#include "src/math_func/Basic_Math.h"
+#include "../src/math_func/Basic_Math.h"
 
-#include "src/trig/trigonometric.h"
+#include "../src/trig/trigonometric.h"
 
-#include "src/vec/vec2.h"
-#include "src/vec/vec3.h"
-#include "src/vec/vec4.h"
+#include "../src/vec/vec2.h"
+#include "../src/vec/vec3.h"
+#include "../src/vec/vec4.h"
 
-#include "src/mat/mat4.h"
+#include "../src/mat/mat4.h"
 
 #endif // !MAR_MATH_MAIN_H
