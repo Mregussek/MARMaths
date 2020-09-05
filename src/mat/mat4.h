dcd7fbb1791e0d74f9fbe81cba9eb3da50d2c1fd
Mateusz Rzeczyca Win <mateuszrzeczyca17@gmail.com>
2020-09-05 22:29:05 +0200

    added GNU-GPL 3.0 license

--- src/mat/mat4.h
+++ src/mat/mat4.h
@@ -1,12 +1,29 @@
-/*
- *	Mateusz Rzeczyca
- *	Copyright (C) 2020 Mateusz Rzeczyca <info@mateuszrzeczyca.pl>
- */
+/**
+ *        MARMaths - open source computing library for MAREngine
+ * Copyright (C) 2020-present Mateusz Rzeczyca <info@mateuszrzeczyca.pl>
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+**/
+
 
 #ifndef MAR_MATH_MAT_4_H
 #define MAR_MATH_MAT_4_H 
 
+
 #include "../maths.h"
+
 
 namespace mar {
     namespace maths {
